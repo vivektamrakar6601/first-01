10bc327cd4c0e8aa393691dad0a99de06ce61bd3
vivek tamrakar <vivektamrakar761@gmail.com>
2025-12-23 18:38:32 +0530

    Add constant x to PersonRepository

--- First/src/main/java/practice/First/repository/PersonRepository.java
+++ First/src/main/java/practice/First/repository/PersonRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import practice.First.entity.Person;
 
 public interface PersonRepository extends JpaRepository<Person,Long> {
+  int x=100;
 
 }
